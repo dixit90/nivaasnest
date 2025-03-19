7071b6388bba28c9f7f5f586b119a5b74d55dd2b
dixit90 <kumarroutraydixit@gmail.com>
2025-03-19 20:10:56 +0530

    first commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+const Footer = () => {
+    return (
+        <footer className="fixed bottom-0 w-full py-2 text-center bg-neutral-100 z-10">
+            Made with ❤️ by <a className="cursor-pointer font-semibold hover:underline" href="https://www.linkedin.com/in/dixitkumarroutray/" rel="nofollow noopener" target="_blank">Dixit</a>
+
+            <a className="cursor-pointer font-semibold hover:underline" href="https://www.linkedin.com/in/akshyat-dash/" rel="nofollow noopener" target="_blank"> Akshat</a>
+
+            
+            <a className="cursor-pointer font-semibold hover:underline" href="https://www.linkedin.com/in/sriman-rabisankar/" rel="nofollow noopener" target="_blank"> Sriman</a>
+        </footer>
+    )
+}
+export default Footer;
