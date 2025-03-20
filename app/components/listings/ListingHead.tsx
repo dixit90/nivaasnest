dddee0c5dbf08cd637e0d42f28bde9a8b9851a7c
dixit90 <kumarroutraydixit@gmail.com>
2025-03-21 00:23:53 +0530

    forth commit

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -5,6 +5,7 @@ import useCountries from "@/app/hooks/useCountries";
 import Heading from "@/app/components/Heading";
 import Image from "next/image";
 import HeartButton from "@/app/components/HeartButton";
+import { FaVrCardboard } from "react-icons/fa6";
 
 interface ListingHeadProps {
     id: string;
@@ -13,6 +14,7 @@ interface ListingHeadProps {
     imageSrc: string;
     currentUser?: SafeUser | null
 }
+
 const ListingHead = ({
      id,
      title,
@@ -23,22 +25,34 @@ const ListingHead = ({
     const { getByValue } = useCountries();
     const location = getByValue(locationValue);
 
-
     return (
         <>
             <Heading
                 title={title}
-                subTitle={`${location?.region}, ${location?.value}`}
+                subtitle={`${location?.region}, ${location?.label}`}
             />
-            <div className="relative w-full h-[60vh] overflow-hidden rounded-xl">
+            <div
+                className="w-full h-[60vh] overflow-hidden rounded-xl relative"
+            >
                 <Image
+                    alt="Image"
                     src={imageSrc}
-                    alt={title}
                     fill
                     className="object-cover w-full"
                 />
-
-                <div className="absolute top-5 right-5">
+                <div className="absolute top-5 right-5 flex flex-row gap-2">
+                    <div 
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            // Your VR toggle function here
+                        }}
+                        className="relative hover:opacity-80 transition cursor-pointer bg-white rounded-full p-1.5"
+                    >
+                        <FaVrCardboard 
+                            size={20} 
+                            className="text-neutral-700"
+                        />
+                    </div>
                     <HeartButton
                         listingId={id}
                         currentUser={currentUser}
